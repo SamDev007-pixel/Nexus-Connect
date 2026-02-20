41cb98e40d73d30566064269ddd270f3e4982847
Sam Devaraja <samdevaraja.j.2024.cse@rajalakshmi.edu.in>
2026-02-20 13:38:42 +0530

    Fixed routing properly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,21 @@
-import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 import SuperAdmin from "./pages/SuperAdmin";
 import Admin from "./pages/Admin";
 import ChatRoom from "./pages/ChatRoom";
 import Broadcast from "./pages/Broadcast";
-import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/super-admin" element={<SuperAdmin />} />
-  <Route path="/admin" element={<Admin />} />
-  <Route path="/chat" element={<ChatRoom />} />
-  <Route path="/broadcast" element={<Broadcast />} />
-</Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/super-admin" element={<SuperAdmin />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/chat" element={<ChatRoom />} />
+        <Route path="/broadcast" element={<Broadcast />} />
+      </Routes>
     </Router>
   );
 }
